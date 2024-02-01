2ba4c900af6f59c84e487d2ddd522fe540b2bfc1
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-01 16:23:34 +0330

    Add ToggleButton component

---
+++ components/ToggleButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex justify-between items-center">
+        <UToggle v-model="(btnModel as boolean)" />
+        <div class="font-medium text-gray-700 dark:text-gray-200">
+            {{ title }}
+        </div>
+    </div>
+    <div @if="description" class="text-sm text-gray-500 dark:text-gray-400 mt-4">
+        {{ description }}
+    </div>
+</template>
+
+<script setup lang="ts">
+const btnModel = defineModel()
+
+const props = defineProps<{
+    title: string;
+    description?: string;
+}>()
+</script>
