8685bb012039f9bc51746f6321c06aac81d02efe
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-03 15:31:06 +0330

    Refactor ThemeSwitch component to improve code readability

--- components/ThemeSwitch.vue
+++ components/ThemeSwitch.vue
@@ -1,9 +1,7 @@
 <template>
     <button id="theme-switch-button" class="btn-nav" @click="toggle">
-        <SunIcon v-if="colorMode.preference === 'dark'" class="h-6 w-6" aria-hidden="true" />
-
-        <MoonIcon v-else class="h-6 w-6" aria-hidden="true" />
-
+        <SunIcon class="hidden dark:block h-6 w-6" aria-hidden="true" />
+        <MoonIcon class="dark:hidden h-6 w-6" aria-hidden="true" />
     </button>
 </template>
   
@@ -14,6 +12,7 @@ import {
 } from '@heroicons/vue/24/outline'
 const colorMode = useColorMode()
 
+let theme = computed(() => colorMode.preference)
 
 const toggle = () => {
     if (colorMode.preference == 'dark')
