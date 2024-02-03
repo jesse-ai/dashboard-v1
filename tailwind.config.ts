2b8141ab930c6235382171a7dd16c387a2584d64
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-03 16:07:36 +0330

    Add .btn-nav class to main.css and update colors in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type {Config} from 'tailwindcss'
+import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>>{
@@ -11,6 +11,17 @@ export default <Partial<Config>>{
                 display: ["Inter"],
                 body: ["Inter"],
             },
+            colors: {
+                primary: {
+                    DEFAULT: '#4f46e5',
+                    dark: '#f9b537'
+                },
+                backdrop: {
+                    DEFAULT: '#ffffff',
+                    dark: '#333333'
+                },
+            },
         },
+
     },
 }
