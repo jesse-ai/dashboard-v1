07f459e3ab3230d0fccc20650f2671f6398ea939
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-04 18:36:41 +0330

    Add Feedback component
    Refactor form properties in Feedback.vue

---
+++ components/Feedback.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="w-full">
+        <p id="feedback-description">
+            I would love to hear your feedback whether it's about a bug, suggestion, something you like, or something you hate about Jesse!
+        </p>
+
+        <br>
+
+        <UForm :state="form" class="space-y-4" @submit="submit">
+            <UFormGroup label="Description" name="Description">
+                <UTextarea v-model="form.description" :rows="6" placeholder="Type here..." />
+            </UFormGroup>
+
+            <UFormGroup label="Email (must be registered with on Jesse.Trade)" description="Enter your email address for us to know who sent the feedback and possibly reply back to you. It must be the email address of your account on Jesse.Trade" required>
+                <UInput v-model="form.email" placeholder="Email address..." type="email" />
+            </UFormGroup>
+
+            <div class="flex justify-end">
+                <UButton id="feedback-cancel-button" color="gray" variant="link" class="mr-8" label="Cancel" @click="close()" />
+
+                <UButton id="feedback-submit-button" type="submit" class="w-48 flex justify-center " label="Submit" :loading="loadingBtn" :disabled="!form.description.length || (!form.email.length && !hasLivePluginInstalled)" />
+            </div>
+        </UForm>
+
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/authState';
+
+const feedback = defineModel()
+const loadingBtn = ref(false);
+const store = useAuthStore();
+const form = reactive({
+    email: '',
+    description: '',
+});
+
+const hasLivePluginInstalled = computed(() => store.hasLivePluginInstalled);
+
+async function submit() {
+    loadingBtn.value = true;
+    const { data, error } = await usePostApi('/feedback', {
+        description: form.description,
+        email: form.email,
+    }, true);
+    loadingBtn.value = false;
+    if (error.value && error.value.statusCode !== 200) {
+        showNotification('error', `[${error.value.statusCode}]: Something went wrong. If this persists, please contact support.`);
+    }
+    // todo: fix type of the response
+    const res = data.value as any;
+    if (res.data.status === 'success') {
+        form.description = '';
+        form.email = '';
+        showNotification('success', res.data.message)
+        close();
+    } else if (res.data.status === 'error') {
+        showNotification('error', res.data.message)
+    }
+}
+
+function close() {
+    feedback.value = false;
+}
+</script>
+  
