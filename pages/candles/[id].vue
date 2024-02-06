0d3bb0c94f29010359cf3d603d67d50d89afd9e3
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-06 15:31:32 +0330

    Add Tabs component and related functionality

---
+++ pages/candles/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <Tabs :page-id="pageId" :tabs="tabs" />
+    
+
+    <!-- <CandlesTab v-if="tabs[pageId]" :form="tabs[pageId].form" :results="tabs[pageId].results" /> -->
+</template>
+  
+<script setup lang="ts">
+import { useCandlesStore } from '@/stores/candleState'
+
+const candlesStore = useCandlesStore()
+const tabs = computed(() => candlesStore.tabs)
+const route = useRoute()
+const pageId = computed(() => parseInt(route.params.id as string))
+</script>
+  
---
+++ pages/candles/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const router = useRouter()
+onMounted(() => {
+    router.push({ path: '/candles/1' })
+})
+</script>
+  
