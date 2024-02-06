8d63e6113c7014e0fd04eeea6f920cdc3a0558fd
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-06 18:22:39 +0330

    Add Divider component

---
+++ components/Divider.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="w-full flex justify-between items-center">
+        <div class="relative flex justify-start bg-inherit z-10">
+            <span class="pr-3 text-lg font-medium text-gray-900 dark:text-gray-100 whitespace-nowrap" :class="backgroundClasses" v-text="title" />
+        </div>
+
+        <div class="w-full border-t-2 border-dashed border-gray-300 dark:border-gray-600" />
+    </div>
+</template>
+  
+  
+<script setup lang="ts">
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    bgLight: {
+        type: String,
+        default: 'bg-white'
+    },
+    bgDark: {
+        type: String,
+        default: 'dark:bg-backdrop-dark'
+    }
+})
+
+const backgroundClasses = computed(() => {
+    return props.bgLight + ' ' + props.bgDark
+})
+</script>
+  
