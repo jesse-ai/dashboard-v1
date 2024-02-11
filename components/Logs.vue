1b3c5e42424bea1685336538d590aa649f557d31
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-11 14:06:31 +0330

    Add EmptyBox and Logs components

---
+++ components/Logs.vue
@@ -0,0 +1,17 @@
+<template>
+    <pre v-if="logs.length" class="whitespace-pre-line rounded border dark:border-gray-600 bg-gray-50 dark:bg-gray-700 select-text text-base dark:text-gray-300 w-full px-4 sm:px-6 py-2" v-html="logs" />
+
+    <EmptyBox v-else />
+</template>
+  
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import helpers from '@/utils/helpers'
+
+defineProps<{
+    logs: string;
+}>()
+
+const timestampToTimeRef = helpers.timestampToTime
+</script>
+  
