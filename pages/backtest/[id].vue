68a061c1550cb3723ee3daf3bec78c06a3550011
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-11 16:18:36 +0330

    Add backtest page and component

---
+++ pages/backtest/[id].vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-full">
+        <Tabs :page-id="pageId" :tabs="tabs" mode="backtest" />
+    </div>
+
+    <BacktestTab v-if="tabs[pageId]" :form="tabs[pageId].form" :results="tabs[pageId].results" />
+</template>
+  
+<script setup lang="ts">
+import { useBacktestStore } from '@/stores/backtestState'
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const backtestsStore = useBacktestStore()
+const tabs = computed(() => backtestsStore.tabs)
+const route = useRoute()
+const pageId = computed(() => parseInt(route.params.id as string))
+</script>
+  
