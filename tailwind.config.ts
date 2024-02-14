a20708fffd2fde032e3a4e8fe52547c37553d78c
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-14 13:42:24 +0330

    Add new colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
+const colors = require('tailwindcss/colors')
 
 export default <Partial<Config>>{
     theme: {
@@ -16,6 +17,19 @@ export default <Partial<Config>>{
                 body: ["Inter"],
             },
             colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                black: colors.black,
+                green: colors.teal,
+                red: colors.red,
+                white: colors.white,
+                gray: colors.trueGray,
+                indigo: colors.indigo,
+                yellow: colors.amber,
+                'cool-gray': colors.coolGray,
+                pink: colors.pink,
+                blue: colors.blue,
+                purple: colors.purple,
                 primary: {
                     DEFAULT: '#4f46e5',
                     dark: '#f9b537'
