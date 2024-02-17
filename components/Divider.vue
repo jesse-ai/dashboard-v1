4c6260d520abf19b73f53569985fd742f07770eb
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-17 15:43:19 +0330

    Remove unused backgroundClasses computed property

--- components/Divider.vue
+++ components/Divider.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full flex justify-between items-center">
         <div class="relative flex justify-start bg-inherit z-10">
-            <span class="pr-3 text-lg font-medium text-gray-900 dark:text-gray-100 whitespace-nowrap" :class="backgroundClasses" v-text="title" />
+            <span class="pr-3 text-lg font-medium text-gray-900 dark:text-gray-100 whitespace-nowrap" v-text="title" />
         </div>
 
         <div class="w-full border-t-2 border-dashed border-gray-300 dark:border-gray-600" />
@@ -14,19 +14,7 @@ const props = defineProps({
     title: {
         type: String,
         required: true
-    },
-    bgLight: {
-        type: String,
-        default: 'bg-white'
-    },
-    bgDark: {
-        type: String,
-        default: 'dark:bg-backdrop-dark'
     }
-})
-
-const backgroundClasses = computed(() => {
-    return props.bgLight + ' ' + props.bgDark
 })
 </script>
   
