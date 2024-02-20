5316f9155cf236409688ae00ce5886d74e3092a6
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-20 18:49:14 +0330

    Add live page components

---
+++ pages/live/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <LiveTab v-if="tabs[pageId]" :form="tabs[pageId].form" :results="tabs[pageId].results" />
+</template>
+  
+<script setup lang="ts">
+import { useLiveStore } from '@/stores/liveState'
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const liveStore = useLiveStore()
+const tabs = computed(() => liveStore.tabs)
+const route = useRoute()
+const pageId = computed(() => parseInt(route.params.id as string))
+</script>
+  
