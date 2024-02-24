cd5774084d539b504f15d9be86e816995cf269c3
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-02-24 11:23:22 +0330

    Add Tooltip component

---
+++ components/Tooltip.vue
@@ -0,0 +1,23 @@
+<template>
+    <span class="relative underline" @mouseenter="showIt" @mouseleave="hideIt">
+        <span v-show="show" class="absolute -top-10 z-90 bg-gray-900 rounded px-2 py-1 text-sm text-white" v-text="title" />
+        <slot />
+    </span>
+</template>
+  
+<script setup lang="ts">
+const props = defineProps<{
+    title: string;
+}>();
+
+const show = ref(false);
+
+const showIt = () => {
+    show.value = true;
+};
+
+const hideIt = () => {
+    show.value = false;
+};
+</script>
+  
