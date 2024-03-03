f8384b8d9582dac26f1799d20a63269cc32b6c97
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-03-03 09:24:55 +0330

    add loading component

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,13 +1,26 @@
 <template>
-    <div class="w-screen h-screen flex justify-center items-center">
-        <div>
-            sdcdscds
-        </div>
+    <div class="w-screen h-screen flex justify-center items-center -mt-16">
+        <div class="w-1/2" ref="animationContainer"></div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import lottie from 'lottie-web';
 
+const animationContainer = ref(null);
+
+onMounted(() => {
+    if (animationContainer.value !== null) {
+        lottie.loadAnimation({
+            container: animationContainer.value, // the DOM element that will contain the animation
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: '/_nuxt/assets/json/loading.json' // the path to the animation json
+        })
+    }
+});
 </script>
 
 <style scoped></style>
