c0bd98fef764ed48419608789d730b9ef9629af2
Saleh Mir <saleh@jesse.trade>
2024-03-25 23:41:41 +0330

    Refactor Loading.vue component

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,59 +1,55 @@
 <template>
-    <div class="mt-8">
-        <LayoutsSidebar>
-            <template #left>
-                <div v-for="i in 4" :class="i == 1 ? '' : 'mt-8'">
-                    <div class="loading-bar h-6 bg-gray-200 dark:bg-gray-700 rounded-full"></div>
-                    <div class="loading-bar2 h-6 bg-gray-200 dark:bg-gray-700 rounded-full mt-4"></div>
-                </div>
-            </template>
+  <div class="mt-8">
+    <LayoutsSidebar>
+      <template #left>
+        <div v-for="i in 4" :class="i === 1 ? '' : 'mt-8'">
+          <div class="loading-bar h-6 bg-gray-200 dark:bg-gray-700 rounded-full"></div>
+          <div class="loading-bar2 h-6 bg-gray-200 dark:bg-gray-700 rounded-full mt-4"></div>
+        </div>
+      </template>
 
-            <template #right>
-                <div class="w-full relative animate-pulse h-12 bg-gray-200 dark:bg-gray-700 rounded-full">
-                    <div class="absolute inset-0">
-
-                    </div>
-                </div>
-                <div class="w-full animate-pulse h-12 bg-gray-200 dark:bg-gray-700 rounded-full mt-4"></div>
-            </template>
-        </LayoutsSidebar>
-    </div>
+      <template #right>
+        <div class="w-full relative animate-pulse h-12 bg-gray-200 dark:bg-gray-700 rounded-full">
+          <div class="absolute inset-0" />
+        </div>
+        <div class="w-full animate-pulse h-12 bg-gray-200 dark:bg-gray-700 rounded-full mt-4"></div>
+      </template>
+    </LayoutsSidebar>
+  </div>
 </template>
 
 <style scoped>
 .loading-bar {
-    width: 0;
-    animation: load 1s infinite, pulse 2s infinite;
+  width: 0;
+  animation: load 1s infinite, pulse 2s infinite;
 }
 
 .loading-bar2 {
-    width: 0;
-    animation: load 4s infinite, pulse 2s infinite;
+  width: 0;
+  animation: load 2s infinite, pulse 2s infinite;
 }
 
 @keyframes load {
-    0% {
-        width: 0;
-    }
+  0% {
+    width: 0;
+  }
 
-    70% {
-        width: 100%;
-    }
+  70% {
+    width: 100%;
+  }
 
-    100% {
-        width: 0;
-    }
+  100% {
+    width: 0;
+  }
 }
 
 @keyframes pulse {
-
-    0%,
-    100% {
-        opacity: 1;
-    }
-
-    50% {
-        opacity: 0.5;
-    }
+  0%,
+  100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
 }
 </style>
