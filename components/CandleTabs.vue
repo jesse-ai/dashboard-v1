1c60c269bbf2b8de57a1029ef1382b7c28f5816b
Saleh Mir <saleh@jesse.trade>
2024-03-27 01:06:45 +0330

    Update CandleTabs.vue styling

--- components/CandleTabs.vue
+++ components/CandleTabs.vue
@@ -3,7 +3,7 @@
         <div class="hidden sm:block">
             <nav class="relative rounded-lg shadow flex divide-x divide-gray-200 dark:divide-gray-700" aria-label="props.Tabs">
                 <div v-for="(tab, name, index) in props.tabs" :key="tab.id" :data-cy="'tab' + index" class="relative group min-w-0 flex-1 overflow-hidden text-center flex items-center ">
-                    <NuxtLink :to="`/candles/${tab.id}`" :class="[tab.id === props.pageId ? 'text-gray-900 dark:text-gray-100 font-bold ' : 'text-gray-500 dark:text-gray-300 hover:text-gray-700 font-medium ', 'py-4 px-4 inline-block select-none cursor-pointer focus:outline-none  w-full text-sm bg-white dark:bg-backdrop-dark hover:bg-gray-50 dark:hover:bg-gray-800']">
+                    <NuxtLink :to="`/candles/${tab.id}`" :class="[tab.id === props.pageId ? 'text-gray-900 dark:text-gray-100 font-bold ' : 'text-gray-500 dark:text-gray-300 hover:text-gray-700 font-medium ', 'py-3 px-4 inline-block select-none cursor-pointer focus:outline-none  w-full text-sm bg-white dark:bg-backdrop-dark hover:bg-gray-50 dark:hover:bg-gray-800']">
                         <span>
                             {{ tab.results.executing ? '' : `Tab ${index + 1}` }} {{ tab.results.executing ? `${tab.form.symbol}` : `` }} {{ tab.results.executing && !tab.results.showResults ? ' | ' + tab.results.progressbar.current + '%' : '' }} {{ tab.results.showResults ? ' - Results' : '' }}
                         </span>
@@ -18,7 +18,7 @@
                 </div>
 
                 <!-- New Tab Button-->
-                <div class="select-none cursor-pointer text-gray-400 dark:text-gray-100 hover:text-gray-600 focus:outline-none group relative w-14 overflow-hidden bg-white dark:bg-backdrop-dark py-4 px-4 font-medium hover:bg-gray-50 dark:hover:bg-gray-800 flex items-center justify-center " @click="addTab">
+                <div class="select-none cursor-pointer text-gray-400 dark:text-gray-100 hover:text-gray-600 focus:outline-none group relative w-14 overflow-hidden bg-white dark:bg-backdrop-dark py-3 px-4 font-medium hover:bg-gray-50 dark:hover:bg-gray-800 flex items-center justify-center " @click="addTab">
                     <button class="absolute right-[1em] focus:outline-none ">
                         <PlusIcon class="h-6 w-6 rounded-full" aria-hidden="true" />
                     </button>
@@ -54,4 +54,3 @@ const closeTab = (tabId: number) => {
     }
 }
 </script>
-  
