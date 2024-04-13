43c21c2e24cdeccdd0aeaafc7247521482779223
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-04-13 15:12:11 +0330

    Update ESLint configuration and modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,35 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // your custom flat configs go here, for example:
-  // {
-  //   files: ['**/*.ts', '**/*.tsx'],
-  //   rules: {
-  //     'no-console': 'off' // allow console.log in TypeScript files
-  //   }
-  // },
-  // {
-  //   ...
-  // }
+  {
+    rules: {
+      'no-console': 'off', // allow console.log in TypeScript files
+      'vue/no-multiple-template-root': 'off',
+      '@stylistic/indent': ['error', 2],
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/html-closing-bracket-newline': 'off',
+      '@stylistic/no-multiple-empty-lines': 0,
+      '@stylistic/no-trailing-spaces': 2,
+      '@stylistic/space-before-function-paren': ['error', {
+        anonymous: 'always',
+        named: 'always',
+        asyncArrow: 'always',
+      }],
+      'prefer-const': 'error',
+      '@stylistic/padded-blocks': 'warn',
+      '@stylistic/comma-dangle': 0,
+      'no-unused-vars': 'warn',
+      'vue/no-unused-vars': 'error',
+      'vue/no-unused-components': 'error',
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 4,
+        multiline: {
+          max: 1,
+        },
+      }],
+      '@stylistic/no-extra-semi': 'warn',
+      'vue/no-mutating-props': 'off',
+      'vue/no-v-html': 'off',
+    },
+  },
 )
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // environment variables
-    runtimeConfig: {
-        ssr: false,
-        public: {
-            apiBaseUrl: process.env.NUXT_API_BASE_URL1,
-            appName: 'Jesse',
-            appUrl: process.env.APP_URL,
-            wsUrl: process.env.VUE_APP_SOCKET_PATH,
-        }
+  // environment variables
+  runtimeConfig: {
+    ssr: false,
+    public: {
+      apiBaseUrl: process.env.NUXT_API_BASE_URL1,
+      appName: 'Jesse',
+      appUrl: process.env.APP_URL,
+      wsUrl: process.env.VUE_APP_SOCKET_PATH,
     },
-    devtools: { enabled: true },
-    modules: [
-        '@pinia/nuxt',
-        '@pinia-plugin-persistedstate/nuxt',
-        '@nuxt/ui',
-        'nuxt-vitest',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/color-mode',
-        '@nuxt/eslint'
-    ],
-    css: [
-        '@/assets/css/main.css'
-    ],
-    router: {
-        options: {
-            hashMode: true
-        }
-    }
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxt/ui',
+    'nuxt-vitest',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@nuxt/eslint',
+  ],
+  css: [
+    '@/assets/css/main.css',
+  ],
+  router: {
+    options: {
+      hashMode: true,
+    },
+  }
 })
