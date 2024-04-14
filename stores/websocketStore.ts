487816a57a6646e1b680da8dec53d9db6b163d73
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-04-14 15:07:25 +0330

    Refactor WebSocket initialization and event handling in websocketStore.ts

--- stores/websocketStore.ts
+++ stores/websocketStore.ts
@@ -4,34 +4,34 @@ import socketActions from '@/utils/socketActions'
 
 
 export const useSocketStore = defineStore('socket', {
-    state: () => ({
-        isConnected: false,
-        socket: {} as WebSocket
-    }),
-    actions: {
-        initiate() {
-            this.socket = new WebSocket(`${useRuntimeConfig().public.wsUrl}?token=${useMainStore().authToken}`);
-            this.socket.addEventListener('open', (event) => {
-                this.isConnected = true
+  state: () => ({
+    isConnected: false,
+    socket: {} as WebSocket
+  }),
+  actions: {
+    initiate () {
+      this.socket = new WebSocket(`${useRuntimeConfig().public.wsUrl}?token=${useMainStore().authToken}`)
+      this.socket.addEventListener('open', () => {
+        this.isConnected = true
 
-                this.socket.addEventListener('message', async (message) => {
-                    const parsedData = JSON.parse(message.data);
-                    const event = parsedData.event
-                    const data = parsedData.data
-                    const idt = parsedData.id
-                    const actions = socketActions().get(event)
+        this.socket.addEventListener('message', async (message) => {
+          const parsedData = JSON.parse(message.data)
+          const event = parsedData.event
+          const data = parsedData.data
+          const id = parsedData.id
+          const actions = socketActions().get(event)
 
-                    if (actions !== undefined) {
-                        actions.forEach(action => {
-                            action(id, data)
-                        })
-                    }
-                })
-            });
-        },
-        disconnect() {
-            this.socket.close()
-            this.isConnected = false
-        }
+          if (actions !== undefined) {
+            actions.forEach((action) => {
+              action(id, data)
+            })
+          }
+        })
+      })
+    },
+    disconnect () {
+      this.socket.close()
+      this.isConnected = false
     }
+  }
 })
