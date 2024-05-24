85ca4b41256ffd838e01ee54eb79081a5db692c0
Saleh Mir <saleh@jesse.trade>
2024-05-24 20:03:04 +0330

    refactor: Update EmptyBox and SmallContainer components for better styling and consistency

--- components/EmptyBox.vue
+++ components/EmptyBox.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="text-center rounded border-2 border-dashed dark:border-gray-800 bg-gray-100 dark:bg-gray-700 py-4 opacity-40 dark:opacity-75 select-none">
-    Empty
+  <div
+    class="text-center rounded border-2 border-dashed border-gray-400 dark:border-gray-700 bg-gray-100 dark:bg-gray-800 py-4 text-sm">
+    <slot />
   </div>
 </template>
---
+++ components/SmallContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="py-10 mx-auto">
+    <main>
+      <div class="mx-auto max-w-7xl px-2 md:px-8">
+        <div class="max-w-[600px] mx-auto">
+          <slot />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
