e4c46fe1cd42ca378086b1a2131e368afe5c933a
Saleh Mir <saleh@jesse.trade>
2024-05-27 00:17:37 +0330

    refactor: Update EmptyBox component background color for better visibility

--- components/EmptyBox.vue
+++ components/EmptyBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="text-center rounded border-2 border-dashed border-gray-400 dark:border-gray-700 bg-gray-100 dark:bg-gray-800 py-4 text-sm">
+    class="text-center rounded border-2 border-dashed border-gray-400 dark:border-gray-700 bg-gray-50 dark:bg-gray-800 py-4 text-sm">
     <slot />
   </div>
 </template>
