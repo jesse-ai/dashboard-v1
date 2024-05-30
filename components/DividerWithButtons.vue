3b77f482c3eb418a9edd7748caab64e3ce79a56f
Saleh Mir <saleh@jesse.trade>
2024-05-30 15:07:13 +0330

    refactor: Improve form field layout and placeholder text in CandlesTab.vue

--- components/DividerWithButtons.vue
+++ components/DividerWithButtons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex justify-between items-center">
-    <span class="pr-3 bg-white dark:bg-backdrop-dark text-lg font-medium text-gray-900 dark:text-gray-200" v-text="title" />
+    <span class="pr-3 bg-white dark:bg-backdrop-dark text-lg font-semibold text-gray-900 dark:text-gray-200" v-text="title" />
 
     <div class="w-full flex items-center" aria-hidden="true">
       <div class="w-full border-t-2 border-dashed border-gray-300 dark:border-gray-600" />
