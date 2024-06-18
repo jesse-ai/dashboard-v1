6d5058e02087ed6b84bcc30de534ba1163a22f07
Saleh Mir <saleh@jesse.trade>
2024-06-19 00:18:53 +0330

    refactor: Update EmptyBox component styling for better visual consistency

--- components/EmptyBox.vue
+++ components/EmptyBox.vue
@@ -1,6 +1,10 @@
 <template>
   <div
-    class="text-center rounded border-2 border-dashed border-gray-400 dark:border-gray-700 bg-gray-50 dark:bg-gray-800 py-4 text-sm">
-    <slot />
+    class="text-center rounded border-2 border-dashed border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-800 py-4 text-sm">
+    <slot>
+      <span class="text-gray-400">
+        Empty
+      </span>
+    </slot>
   </div>
 </template>
